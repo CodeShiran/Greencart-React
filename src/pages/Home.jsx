eaaca702479b1a1425deda81bb3d6f22e91a3e1d
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-05-26 19:53:39 +0530

    categories completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainBanner from '../context/MainBanner'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner />
+        <Categories />
     </div>
   )
 }
