85bdddece2466c3970cf75fbd2f19b1c2cf8176c
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-05-29 07:29:04 +0530

    best sellers completed

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,66 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = ({children}) => {
 
+    const currency = import.meta.VITE_Currency
+
     const navigate = useNavigate()
     const [user, setUser] = useState(null)
     const [isSeller, setIsSeller] = useState(false)
     const [showUserLogin, setShowUserLogin] = useState(false)
+    const [products, setProducts] = useState([])
+    const [cartItems, setCartItems] = useState({})
 
-    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin}
+    //fetch all product
+    const fetchProducts = async () => {
+        setProducts(dummyProducts)
+    }
+
+    //add products to cart
+    const addToCart = (itemId) => {
+        let cartData = structuredClone(cartItems)
+
+        if(cartData[itemId]) {
+            cartData[itemId] += 1
+        }
+        else {
+            cartData[itemId] = 1
+        }
+        setCartItems(cartData)
+        toast.success("added to cart")
+    }
+
+    //update cart items
+    const updateCartItems = (itemId, quantity) => {
+        let cartData = structuredClone(cartItems)
+        cartData[itemId] = quantity
+        setCartItems(cartData)
+        toast.success("cart updated")
+    }
+
+    //remove product from cart
+    const removeFromCart = (itemId) => {
+        let cartData = structuredClone(cartItems)
+        if(cartData[itemId]) {
+            cartData[itemId] -= 1
+            if(cartData[itemId] === 0) {
+                delete cartData[itemId]
+            }
+            toast.success("remove from cart")
+            setCartItems(cartData)
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    },[])
+
+    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItems, removeFromCart, cartItems}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
